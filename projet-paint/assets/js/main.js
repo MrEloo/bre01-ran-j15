a7c14c4d6e2acb0940f55fd2daca93eb957bac56
Eloan <nidnioc.coindin@outlook.fr>
2023-11-03 17:25:41 +0000

    envoie des exercices + bonus

--- projet-paint/assets/js/main.js
+++ projet-paint/assets/js/main.js
@@ -21,18 +21,34 @@ function loadPalette(palette)
 {
     let colors = palette;
 
-    let divs = document.querySelectorAll("header > div")
+    
     
 
     for(let i = 0; i < palette.length; i++){
-        divs[i].style.backgroundColor = palette[i]
+        let header = document.querySelector("header")
+        let div = document.createElement("div")
+        let input = document.createElement("input")
+        input.type = "color"
+        input.id = `color-input-${i}`
+        div.style.backgroundColor = palette[i]
+        
+        header.appendChild(div)
+        div.appendChild(input)
+        
+        input.addEventListener("input", (e)=>{
+            let selectedColor = e.target.value
+            div.style.backgroundColor = selectedColor
+        })
+        
+        
     }
+    
     return palette
 }
 
 
 window.addEventListener("DOMContentLoaded", function(){
-    loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80",]);
+    loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80", "#A4B494", "#BEC5AD", "#3B5249", "#519872", "#34252F", "#5C0029", "#61304B", "#857C8D"]);
     
     
     let divs = document.querySelectorAll("header > div")
@@ -61,4 +77,44 @@ window.addEventListener("DOMContentLoaded", function(){
             }
     })
     }
+    
+           
+        let bodyMain = document.querySelector("body > main")
+        let MainDiv = document.querySelector("main > div")
+        let vraiMain = document.querySelector("main")
+        
+        
+        let row =  parseInt(prompt("Veuillez choisir un nombre de ligne : "))
+        let col =  parseInt(prompt("Veuillez choisir un nombre de colonne : "))
+        
+
+        
+        for(let i = 0; i < row ; i++){
+            let divRow = document.createElement("div")
+            divRow.id = `row-${i}`
+
+            for(let j = 0; j < col; j++){
+              let divCol = document.createElement("div") 
+              divCol.id = `col-${j}`
+              divRow.appendChild(divCol)
+              
+        
+            divCol.addEventListener("click", ()=>{
+                let color = getSelectedColor(); 
+                if(divCol.style.backgroundColor){
+                    divCol.style.backgroundColor = ""
+                }else {
+                    divCol.style.backgroundColor = color
+                }
+            })
+            }
+            vraiMain.appendChild(divRow)
+        }
+        
+        
+        // // document.body.style.gridTemplateRows = `1fr, ${row}fr`
+        // // bodyMain.style.gridTemplateRows = `repeat(${row}, 1fr)`
+        // // MainDiv.style.gridTemplateColumns = `repeat(${col}, 1fr)`      
+        
+        
 });
